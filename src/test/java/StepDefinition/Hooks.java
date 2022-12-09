dc1b877207f9b55429173fbebab051249ab27370
akumarsing017 <akash.kumar.singh@pwc.com>
2022-12-09 19:33:34 +0530

    RedBus

--- src/test/java/StepDefinition/Hooks.java
+++ src/test/java/StepDefinition/Hooks.java
@@ -3,13 +3,13 @@ package StepDefinition;
 import Manager.DriverManager;
 import Utils.BaseUtils;
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Scenario;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.sql.ResultSet;
@@ -24,10 +24,10 @@ public class Hooks {
         driver = DriverManager.getDriver();
     }
 
-    @After(order = 2)
+    @AfterStep()
     public void capture(Scenario sc) throws IOException {
-        if(sc.isFailed())
-        {
+
+            // One way to take screenshot and save to any folder
             /*
             TakesScreenshot ts = (TakesScreenshot) driver;
             File src = ts.getScreenshotAs(OutputType.FILE);
@@ -36,11 +36,11 @@ public class Hooks {
             FileUtils.copyFile(src, dest);
             */
 
-            // Taking Screenshot
+            // Another way to take screenshot and save to any folder and also attach to extent report
             TakesScreenshot ts = (TakesScreenshot) driver;
-            File src = ts.getScreenshotAs(OutputType.FILE);
-            sc.attach(Files.readAllBytes(src.toPath()), "image/png", "Screenshot");
-        }
+            final byte[] screenshot = ts.getScreenshotAs(OutputType.BYTES);
+            sc.attach(screenshot, "image/png", "screenshot");
+
     }
 
     @After(order = 1)
@@ -48,7 +48,8 @@ public class Hooks {
         ResultSet rs = BaseUtils.executeQuery();
         while(rs.next())
         {
-            System.out.println(rs.getString("name") + "=" + rs.getInt("id"));
+            System.out.println("Query executed successfully");
+            System.out.println(rs.getString("name") + " = " + rs.getInt("id"));
         }
 
         BaseUtils.closeCon();
