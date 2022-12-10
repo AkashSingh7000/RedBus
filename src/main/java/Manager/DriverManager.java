619e4859417f7eccf23888321378a351c93b0eb6
akumarsing017 <akash.kumar.singh@pwc.com>
2022-12-10 17:52:38 +0530

    RedBus

--- src/main/java/Manager/DriverManager.java
+++ src/main/java/Manager/DriverManager.java
@@ -2,8 +2,16 @@ package Manager;
 
 import Utils.BaseUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
 
 public class DriverManager {
 
@@ -17,6 +25,20 @@ public class DriverManager {
             driver = new ChromeDriver();
 
         }
+        else if(browser.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+
+        }
+        else if(browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            FirefoxBinary f = new FirefoxBinary(new File("C:\\Users\\akumarsing017\\AppData\\Local\\Mozilla Firefox\\firefox.exe"));
+            FirefoxOptions op = new FirefoxOptions();
+            op.setBinary(f);
+            driver = new FirefoxDriver(op);
+
+        }
+
         else{
             System.out.println("Enter the valid browser name");
         }
